069c589537dd90685bd4b6d74b9a9a121111a623
= <rafael.borges2698@gmail.com>
2020-09-12 04:20:03 -0300

    add the logo in page login

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -10,7 +10,8 @@ const AuthRoutes: React.FC = () => (
         <Auth.Navigator screenOptions={{
             headerShown: false,
             cardStyle: {backgroundColor: '#312e38'},
-        }} >
+            }} 
+        >
             <Auth.Screen name="SignIn" component={SignIn} />
             <Auth.Screen name="SignUp" component={SignUp} />
         </Auth.Navigator>
